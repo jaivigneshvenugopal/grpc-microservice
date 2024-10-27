e7ed803d8dab56571c298079271f38ded74c5e8e
Jaivignesh Venugopal <jaivigneshvenugopal@gmail.com>
2024-10-27 21:10:21 +0800

    End-to-end server-client-html setup

--- grpc_client/client.py
+++ grpc_client/client.py
@@ -4,13 +4,31 @@ sys.path.insert(0, '../protos')
 import grpc
 import meter_pb2
 import meter_pb2_grpc
+from google.protobuf.json_format import MessageToDict
 
-def run():
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    output = []
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = meter_pb2_grpc.MeterReadingStub(channel)
         responses = stub.IssueMeterReading(meter_pb2.MeterReadingRequest())
         for response in responses:
-            print(response)
+            output.append(MessageToDict(response))
+    
+    return output
 
 if __name__ == "__main__":
-    run()
+    uvicorn.run(app, port=8000)
