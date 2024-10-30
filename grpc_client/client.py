62bbb07d46ec4dfc5746b419ae880831a2b64a01
Jaivignesh Venugopal <jaivigneshvenugopal@gmail.com>
2024-10-30 16:44:24 +0800

    Update model

--- grpc_client/client.py
+++ grpc_client/client.py
@@ -2,13 +2,12 @@ import sys
 sys.path.insert(0, '../protos')
 
 import grpc
+import uvicorn
 import meter_pb2
 import meter_pb2_grpc
-from google.protobuf.json_format import MessageToDict
-
-import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from google.protobuf.json_format import MessageToDict
 
 app = FastAPI()
 app.add_middleware(
@@ -20,15 +19,15 @@ app.add_middleware(
 )
 
 @app.get("/")
-def read_root():
-    output = []
+def root():
+    meter_readings = []
     with grpc.insecure_channel("grpc_server:50051") as channel:
         stub = meter_pb2_grpc.MeterReadingStub(channel)
-        responses = stub.IssueMeterReading(meter_pb2.MeterReadingRequest())
-        for response in responses:
-            output.append(MessageToDict(response))
+        meter_reading_responses = stub.IssueMeterReading(meter_pb2.MeterReadingRequest())
+        for meter_reading_response in meter_reading_responses:
+            meter_readings.append(MessageToDict(meter_reading_response))
     
-    return output
+    return meter_readings
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
--- models/meter_reading.py
+++ models/meter_reading.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, ConfigDict
 from google.protobuf.timestamp_pb2 import Timestamp
 
 class MeterReading(BaseModel):
+    # pydantic does not allow arbitrary types (protobuf Timestamp) by default
     model_config = ConfigDict(arbitrary_types_allowed=True)
     timestamp: Timestamp
     meter_reading_value: float
