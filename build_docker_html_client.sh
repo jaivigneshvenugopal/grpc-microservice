1003a0165c0e3d7f27b80cdf8ca21228b8d9b8e1
Jaivignesh Venugopal <jaivigneshvenugopal@gmail.com>
2024-10-30 13:08:48 +0800

    Set up docker compose for all 3 images

--- build_docker_html_client.sh
+++
@@ -1,4 +0,0 @@
-docker build -t html_client -f html_client/Dockerfile .
-docker tag html_client:latest jaivigneshvenugopal/html_client:latest
-docker push jaivigneshvenugopal/html_client
-docker run --name html_client -p 80:80 jaivigneshvenugopal/html_client
---
+++ build_docker_images.sh
@@ -0,0 +1,9 @@
+docker build -t grpc_server -f grpc_server/Dockerfile .
+docker tag grpc_server:latest jaivigneshvenugopal/grpc_server:latest
+docker push jaivigneshvenugopal/grpc_server
+# docker run --name grpc_server -p 50551:50551 jaivigneshvenugopal/grpc_server
+
+docker build -t grpc_client -f grpc_client/Dockerfile .
+docker tag grpc_client:latest jaivigneshvenugopal/grpc_client:latest
+docker push jaivigneshvenugopal/grpc_client
+# docker run --name grpc_client -p 8080:8080 jaivigneshvenugopal/grpc_client
